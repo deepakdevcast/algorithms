c3fa904a99619a5c270d84672f45bb8d2f95e24a
deepakjuit <dk8232847@gmail.com>
2021-07-16 10:26:00 +0530

    binary recursion

--- searching/binarySearchrec.py
+++ searching/binarySearchrec.py
@@ -4,17 +4,15 @@ def binarySearchrec(arr,left,right,x):
         if(arr[mid]==x):
             return mid
         elif(arr[mid]>x):
-            right = mid-1
             return binarySearchrec(arr,left,mid-1,x)
         elif(arr[mid]<x):
-            left = mid+1
             return binarySearchrec(arr,mid+1,right,x)
-    return None
+    return -1
 
 if __name__ == "__main__":
     # arr = [int(num) for num in input().strip().split(" ")]
     arr = [1,2,3,4,5,6,7,8,9]
     # x = int(input())
-    x=7
+    x=9
     index = binarySearchrec(arr,0,len(arr)-1,x)
-    print(index+1)
+    print(index)
