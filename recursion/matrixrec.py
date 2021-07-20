264ef1888b370498535f4934ad6ee524d7ac05cf
deepakjuit <dk8232847@gmail.com>
2021-07-20 11:25:09 +0530

    up matrix path finder

--- recursion/matrixrec.py
+++ recursion/matrixrec.py
@@ -2,17 +2,16 @@
 #r,d is starting position
 #fr,fd is finding position
 def pathfinder(matrix,fr,fd,r=0,d=0,path=""):
+    if r==len(matrix[0]) or d==len(matrix): return
     if r==fr and d==fd:
         print(path)
         return
-    if r<=len(matrix[0])-1:
-        path+="R"
-        pathfinder(matrix,fr,fd,r+1,d,path)
-        path=path[:-1]
-    if d<=len(matrix)-1:
-        path+="D"
-        pathfinder(matrix,fr,fd,r,d+1,path)
-        path = path[:-1]
+    path+="R"
+    pathfinder(matrix,fr,fd,r+1,d,path)
+    path=path[:-1]
+    path+="D"
+    pathfinder(matrix,fr,fd,r,d+1,path)
+    path = path[:-1]
 
 matrix = [  ["start",2,  3],
             [   4  ,   5 ,"find"],
