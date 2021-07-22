565be624286686c7e07b55010f63ff42d7eb8dd6
deepakjuit <dk8232847@gmail.com>
2021-07-22 15:20:52 +0530

    N queen with hash

--- recursion/Nqueen.py
+++ recursion/Nqueen.py
@@ -2,35 +2,38 @@
 #1. no one attack each other
 #2. only one in row
 #3. only one in col
-def nQueen(matrix,c):
+def nQueen(matrix,c,hashrow,hashdiag,hashdiagU):
     #terminate condition 
     if c==len(matrix[0]):
         print(matrix)
         return
 
     for i in range(len(matrix[0])):
-        if issafe(i,c,matrix):
+        if issafe(i,c,matrix,hashrow,hashdiag,hashdiagU):
             matrix[i][c]=1
-            nQueen(matrix,c+1)
+            hashrow[i]=1
+            hashdiag[i+c]=1
+            hashdiagU[len(matrix)-1+c-i]==1
+            nQueen(matrix,c+1,hashrow,hashdiag,hashdiagU)
             matrix[i][c]=0
+            hashdiagU[len(matrix)-1+c-i]==0
+            hashdiag[i+c]=0
+            hashrow[i]=0
     return
 
 
-#we need to check only three becuause we start filling queen left to right
-def issafe(row,col,matrix):
-    #first direction: upper-left diagonal
-    for i,j in zip(range(row,-1,-1),range(col,-1,-1)):
-        if matrix[i][j]==1: return False
-
+#we need to check only three diagonal becuause we start filling queen left to right
+def issafe(row,col,matrix,hashrow,hashdiag,hashdiagU):
+    if hashdiag[row+col]==1: return False
     #second direction: column or left
-    for i in range(col,-1,-1):
-        if matrix[row][i]==1: return False
-
+    if hashrow[row]==1: return False
     #third direction: lower-left diagonal
-    for i,j in zip(range(row,len(matrix)),range(col,-1,-1)):
-        if matrix[row][i]==1: return False
+    if hashdiagU[len(matrix)-1 + col -row]==1: return False
     return True
 
-
-matrix= [[0 for j in range(4)] for i in range(4)]
-nQueen(matrix,0)
+n=4
+matrix= [[0 for j in range(n)] for i in range(n)]
+hashrow = [0 for i in range(n)]
+hashdiag = [0 for i in range(2*n-1)]
+hashdiagU = [0 for i in range(2*n-1)]
+nQueen(matrix,0,hashrow,hashdiag,hashdiagU)
